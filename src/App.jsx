eebd785e8191a36881f44bce03c59f09546a9038
Lara Reyes <laradreyes@gmail.com>
2023-05-08 09:09:29 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Inicio from './components/inicio/Inicio'
 
 function App() {
   return (
     <>
+    <Inicio/>
     </>
   )
 }
---
+++ src/components/inicio/Inicio.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Inicio.css'
+
+function Inicio() {
+  return (
+    <>
+    <main>
+        <div>algo</div>
+        <div>
+        <button className='registrarse'>Registrarse Gratis</button>
+        <button className='contGoogle'>Continuar con Google</button>
+        <button className='contApple'>Continuar con Apple</button>
+        <button className='iniciarSes'>Iniciar Sesión</button>
+        </div>
+    </main>
+    </>
+  )
+}
+
+export default Inicio
