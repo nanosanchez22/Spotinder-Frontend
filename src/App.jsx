bb2da20bff1ebce27af4ec4439c89884166de2f1
Cristian Mendez <cristianmen7@gmail.com>
2023-05-09 13:30:46 -0300

    componente musicaContextual

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import Inicio from './components/inicio/Inicio'
+import "./App.css";
+import Inicio from "./components/inicio/Inicio";
+import { MusicaContexto } from "./components/musicaContextual/MusicaContexto";
+import { Perfil } from "./components/perfil/Perfil";
 
 function App() {
   return (
     <>
-    <Inicio/>
+      <Perfil />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/musicaContextual/generos.jsx
@@ -0,0 +1,73 @@
+export const Generos = () => {
+  return (
+    <>
+      <p3 className="titulo tituloGeneros">Selecciona hasta 3 géneros:</p3>
+      <div className="botones">
+        <div className="division">
+          <button>
+            <p>Rock</p>
+          </button>
+          <button>
+            <p>Country</p>
+          </button>
+          <button>
+            <p>Soul</p>
+          </button>
+          <button>
+            <p>Jazz</p>
+          </button>
+          <button>
+            <p>Blues</p>
+          </button>
+        </div>
+        <div className="division">
+          <button>
+            <p>Hip-Hop</p>
+          </button>
+          <button>
+            <p>Pop</p>
+          </button>
+          <button>
+            <p>Reggae</p>
+          </button>
+          <button>
+            <p>Folk</p>
+          </button>
+          <button>
+            <p>R&B</p>
+          </button>
+        </div>
+
+        <div className="division">
+          <button>
+            <p>Clásico</p>
+          </button>
+          <button>
+            <p>Alternativo</p>
+          </button>
+          <button>
+            <p>Ambiente</p>
+          </button>
+          <button>
+            <p>EDM</p>
+          </button>
+        </div>
+
+        <div className="division">
+          <button>
+            <p>Electrónica</p>
+          </button>
+          <button>
+            <p>Disco</p>
+          </button>
+          <button>
+            <p>New Age</p>
+          </button>
+          <button>
+            <p>Punk</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
