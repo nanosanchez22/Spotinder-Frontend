6db47c469aa5c786adcaf9510de595ab0ff84e56
CristianMen7 <cristianmen7@gmail.com>
2023-05-12 10:17:21 -0300

    Actualizacion en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import "./App.css";
 import BarraNavegacion from "./components/barraNavegacion/BarraNavegacion";
-<<<<<<< HEAD
-import { BrowserRouter as Router } from "react-router-dom";
-import ContenidoPrincipal from "./routes/ContenidoPrincipal";
-
-function App() {
-  return (
-    <Router>
-      <ContenidoPrincipal></ContenidoPrincipal>
-      <BarraNavegacion />
-    </Router>
-=======
 import Home from "./components/home/Home";
 import Inicio from "./components/inicio/Inicio";
 import { Outlet } from "react-router-dom";
@@ -21,7 +10,6 @@ function App() {
       <Outlet />
       <BarraNavegacion></BarraNavegacion>
     </>
->>>>>>> 3683eeb8d16ee68a6d68e8aba7036870394426d4
   );
 }
 
