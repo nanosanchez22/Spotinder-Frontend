d5ae91ab74cf7f050669cfec968736bf6166cee9
CristianMen7 <cristianmen7@gmail.com>
2023-05-12 10:10:56 -0300

    actualizando mi rama

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import "./App.css";
+import BarraNavegacion from "./components/barraNavegacion/BarraNavegacion";
+import Home from "./components/home/Home";
+import Inicio from "./components/inicio/Inicio";
+import { Outlet } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <>
+      <Outlet />
+      <BarraNavegacion></BarraNavegacion>
+    </>
+  );
+}
+
+export default App;
